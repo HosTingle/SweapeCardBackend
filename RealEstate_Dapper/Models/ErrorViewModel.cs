01e583a59f3a8d5f523554945dbeffb543656c9c
Gökdeniz Genç <93677929+HosTingle@users.noreply.github.com>
2024-07-17 10:17:42 +0300

    Delete RealEstate_Dapper directory

--- RealEstate_Dapper/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RealEstate_Dapper.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
